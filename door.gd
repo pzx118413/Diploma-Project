56b69c813e30ac4bda1fecc4d5d3b7ddcb24b668
pzx118413 <pzx118413@student.poznan.merito.pl>
2026-03-17 14:26:49 +0100

    initial commit from Godot

---
+++ door.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+@onready var area = $Area2D
+@onready var exit_point = $ExitPoint
+
+var player_inside = false
+
+func _ready():
+	area.body_entered.connect(_on_body_entered)
+	area.body_exited.connect(_on_body_exited)
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		player_inside = true
+
+func _on_body_exited(body):
+	if body.name == "Player":
+		player_inside = false
+
+func _process(delta):
+	if player_inside and Input.is_action_just_pressed("ui_accept"):
+		var player = get_node("../Player")
+		player.global_position = exit_point.global_position + Vector2(0, -16)
---
+++ player.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+@export var speed := 200
+
+func _physics_process(delta):
+	var direction = Vector2.ZERO
+
+	if Input.is_action_pressed("ui_right"):
+		direction.x += 1
+	if Input.is_action_pressed("ui_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		direction.y += 1
+	if Input.is_action_pressed("ui_up"):
+		direction.y -= 1
+
+	direction = direction.normalized()
+	velocity = direction * speed
+	move_and_slide()
